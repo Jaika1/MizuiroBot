5aaf061d9655fbc36cfff8cc0222205aaa5dda6b
Jaika1 <jackroennfeldtnospam@gmail.com>
2020-06-27 15:22:45 +0930

    Begun work on adding some fun commands to the bot

---
+++ MizuiroBot/Discord/Commands/FunModule.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MizuiroBot.Discord.Commands
+{
+    public class FunModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("teirlist")]
+        [Summary("Grabs a random teir list from https://tiermaker.com/")]
+        public async Task RandomTeirListCommand()
+        {
+
+        }
+    }
+}
