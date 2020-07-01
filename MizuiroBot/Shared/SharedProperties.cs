de0768cd1ba48414d117c8cdcf5052fbb3320ee1
Jaika1 <jackroennfeldtnospam@gmail.com>
2020-07-01 22:42:32 +0930

    Working on a class that can store info about both a discord and twitch guild/channel

---
+++ MizuiroBot/Shared/SharedProperties.cs
@@ -0,0 +1,16 @@
+﻿using Discord;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MizuiroBot.Shared
+{
+    public static class SharedBotInfo
+    {
+        public static ulong discordGuildId = 0;
+        public static string twitchChannelName = "";
+        public static Dictionary<string, string> customCommands = new Dictionary<string, string>();
+    }
+}
